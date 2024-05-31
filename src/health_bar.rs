37c23d397393a0d1b6adb0bbecd5e77f3b79b51c
jason <jaswged@gmail.com>
2024-05-30 22:14:12 -0500

    Add health_bar inital logic

--- src/health_bar.rs
+++ src/health_bar.rs
@@ -1,7 +1,9 @@
 // change our imports to include what we will use
-use agb::{display::object::{Graphics, Object, OamManaged, Tag}, include_background_gfx};
+use alloc::vec::Vec;
+use agb::{display::object::{Graphics, Object, OamManaged, Tag}};
 use agb::display::Priority;
 use agb::display::tiled::{RegularBackgroundSize, Tiled0, VRamManager};
+use agb::display::object::Sprite;
 
 use crate::game_manager::GRAPHICS;
 
@@ -13,43 +15,38 @@ static HEALTH_MID: &Tag = GRAPHICS.tags().get("hp_filled");
 static HEALTH_MID_EMPTY: &Tag = GRAPHICS.tags().get("hp_empty");
 static HEALTH_BAR_END: &Tag = GRAPHICS.tags().get("hp_end");
 
+static MID_MT_SPRITE: &Sprite = HEALTH_MID_EMPTY.sprite(0);
+static MID_FILL_SPRITE: &Sprite = HEALTH_MID.sprite(0);
+
 pub struct HealthBar<'obj> {
     health_letters: Object<'obj>,
     health_start: Object<'obj>,
-    health_mid1: Object<'obj>,
-    health_mid2: Object<'obj>,
-    health_mid3: Object<'obj>,
-    // health_mt: Object<'obj>,
     health_end: Object<'obj>,
+    middle_healths: Vec<Object<'obj>>
 }
 
 impl<'obj> HealthBar<'obj> {
     pub fn new(object: &'obj OamManaged<'_>, start_x: i32, start_y: i32) -> Self {
         let mut health_letters = object.object_sprite(HEALTH_BAR_LETTERS.sprite(0));
         let mut health_start = object.object_sprite(HEALTH_BAR_START_CONT.sprite(0));
-        let mut health_mid1 = object.object_sprite(HEALTH_CONT.sprite(0));
-        let mut health_mid2 = object.object_sprite(HEALTH_CONT.sprite(0));
-        let mut health_mid3 = object.object_sprite(HEALTH_CONT.sprite(0));
-        // let mut health_mid = object.object_sprite(HEALTH_MID.sprite(0));
-        // let mut health_mt = object.object_sprite(HEALTH_MID_EMPTY.sprite(0));
         let mut health_end = object.object_sprite(HEALTH_BAR_END.sprite(0));
 
         health_letters.show();
         health_start.show();
-        health_mid1.show();
-        health_mid2.show();
-        health_mid3.show();
-        // health_mt.show();
         health_end.show();
+
+        let mut middle_healths: Vec<Object> = Vec::new();
+        for _ in 0..38{
+            let mut tmp = object.object_sprite(HEALTH_MID.sprite(0));
+            tmp.show();
+            middle_healths.push(tmp);
+        }
 
         let mut health_bar = Self {
             health_letters,
             health_start,
-            health_mid1,
-            health_mid2,
-            health_mid3,
-            // health_mt,
             health_end,
+            middle_healths
         };
 
         health_bar.set_position(start_x, start_y);
@@ -72,37 +69,13 @@ impl<'obj> HealthBar<'obj> {
             to_return.push_str(&a);
         }
          */
-        self.health_mid1.set_position((x+16, y));
-        self.health_mid2.set_position((x+24, y));
-        self.health_mid3.set_position((x+32, y));
-        // self.health_mt.set_position((x+14, y));
+        let mut cnt = x + 12;
+        for o in &mut self.middle_healths{
+            o.set_position((cnt, y));
+            cnt += 1;
+        }
 
         // End goes at a fixed distance from start
         self.health_end.set_position((x+40, y)); // 80
     }
-
-    // fn fill_health( vram: &mut VRamManager, tiled: &Tiled0){
-    //     include_background_gfx!(backgrounds, "000000",
-    //     level => deduplicate "gfx/dungeon_floor.png",
-    //     dungeon => deduplicate "gfx/dungeon.aseprite");
-    //
-    //     let tileset = &backgrounds::level.tiles;
-    //     vram.set_background_palettes(backgrounds::PALETTES);
-    //
-    //     let mut bg = tiled.background(Priority::P0,
-    //                                   RegularBackgroundSize::Background32x32,
-    //                                   tileset.format());
-    //     for y in 0..16u16 {
-    //         for x in 0..30u16 {
-    //             bg.set_tile(
-    //                 vram,
-    //                 (x, y),
-    //                 tileset,
-    //                 backgrounds::level.tile_settings[0 as usize],
-    //             );
-    //         }
-    //     }
-    //     bg.commit(vram);
-    //     bg.set_visible(true);
-    // }
 }
