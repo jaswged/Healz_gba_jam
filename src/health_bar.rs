25c42e5264d62f5a3a6309d175b881cb658812c9
Jason <jaswged@gmail.com>
2024-06-01 23:49:16 -0500

    Health bar sprite array

--- src/health_bar.rs
+++ src/health_bar.rs
@@ -7,7 +7,16 @@ use agb::println;
 
 use crate::game_manager::GRAPHICS;
 
-// todo hashmap lookup of int to sprite
+static HP_1_SPRITE: &Sprite = GRAPHICS.tags().get("hp_1").sprite(0);
+static HP_2_SPRITE: &Sprite = GRAPHICS.tags().get("hp_2").sprite(0);
+static HP_3_SPRITE: &Sprite = GRAPHICS.tags().get("hp_3").sprite(0);
+static HP_4_SPRITE: &Sprite = GRAPHICS.tags().get("hp_4").sprite(0);
+static HP_5_SPRITE: &Sprite = GRAPHICS.tags().get("hp_5").sprite(0);
+static HP_6_SPRITE: &Sprite = GRAPHICS.tags().get("hp_6").sprite(0);
+static HP_7_SPRITE: &Sprite = GRAPHICS.tags().get("hp_7").sprite(0);
+static HP_8_SPRITE: &Sprite = GRAPHICS.tags().get("hp_8").sprite(0);
+static HP_SPRITE_ARR: [&Sprite; 8] = [HP_8_SPRITE,HP_7_SPRITE,HP_6_SPRITE,HP_5_SPRITE,HP_4_SPRITE,HP_3_SPRITE,HP_2_SPRITE,HP_1_SPRITE];
+
 
 pub struct HealthBar<'obj> {
     health_amt: usize,
@@ -17,13 +26,12 @@ pub struct HealthBar<'obj> {
     health_mid4: Object<'obj>,
     health_end: Object<'obj>,
     object: &'obj OamManaged<'obj>,
-    hp_sprite_map: &Vec<&Sprite><'obj>
 }
 
 impl<'obj> HealthBar<'obj> {
-    pub fn new(object: &'obj OamManaged<'obj>, hp_sprite_map: &Vec<&Sprite><'obj>, start_x: i32, start_y: i32) -> Self {
+    pub fn new(object: &'obj OamManaged<'obj>, start_x: i32, start_y: i32) -> Self {
         let mut health_amt = 32; // 32 for easy math. set back to 35 if figure out a solution
-        let filled = *hp_sprite_map.get(0).unwrap();
+        let filled = HP_SPRITE_ARR[0];
         let mut health_mid1 = object.object_sprite(filled);
         let mut health_mid2 = object.object_sprite(filled);
         let mut health_mid3 = object.object_sprite(filled);
@@ -43,8 +51,7 @@ impl<'obj> HealthBar<'obj> {
             health_mid3,
             health_mid4,
             health_end,
-            object,
-            hp_sprite_map
+            object
         };
 
         health_bar.set_position(start_x, start_y);
@@ -73,6 +80,9 @@ impl<'obj> HealthBar<'obj> {
         }
         let new_health = self.health_amt - damage;
 
+        // todo. if the damage value passed in is less than 8 you only have to look at 2 blocks
+        // is there an easy way to know if they are in the same block?
+
         // todo match on ranges
         println!("Test ranges where they are the same");
         let mut orig_block = match (self.health_amt, new_health){
@@ -80,7 +90,7 @@ impl<'obj> HealthBar<'obj> {
                 println!("First sprite");
                 // Calculate new sprite off of the new value
                 println!("Diff is {}", 8-new_health);
-                let new_sprite = *self.hp_sprite_map.get(8-new_health).unwrap();
+                let new_sprite = HP_SPRITE_ARR[8-new_health];
                 self.health_mid1.set_sprite(self.object.sprite(new_sprite));
                 (1, &mut self.health_mid1, 8-self.health_amt)
             },
--- src/main.rs
+++ src/main.rs
@@ -70,7 +70,6 @@ static BOSS_SPRITE: &Tag = GRAPHICS.tags().get("boss");
 static BANNER_L_SPRITE: &Tag = GRAPHICS.tags().get("banner_l");
 static BANNER_M_SPRITE: &Tag = GRAPHICS.tags().get("banner_mid");
 
-
 // The main function must take 1 arguments and never return. The agb::entry decorator
 // ensures that everything is in order. `agb` will call this after setting up the stack
 // and interrupt handlers correctly. It will also handle creating the `Gba` struct for you.
@@ -99,26 +98,6 @@ fn game_main(mut gba: agb::Gba) -> ! {
 
     // todo Show bottom banner and initial "story" text
 
-    // Health bar sprites
-    let hp_1_sprite: &Sprite = GRAPHICS.tags().get("hp_1").sprite(0);
-    let hp_2_sprite: &Sprite = GRAPHICS.tags().get("hp_2").sprite(0);
-    let hp_3_sprite: &Sprite = GRAPHICS.tags().get("hp_3").sprite(0);
-    let hp_4_sprite: &Sprite = GRAPHICS.tags().get("hp_4").sprite(0);
-    let hp_5_sprite: &Sprite = GRAPHICS.tags().get("hp_5").sprite(0);
-    let hp_6_sprite: &Sprite = GRAPHICS.tags().get("hp_6").sprite(0);
-    let hp_7_sprite: &Sprite = GRAPHICS.tags().get("hp_7").sprite(0);
-    let hp_8_sprite: &Sprite = GRAPHICS.tags().get("hp_8").sprite(0);
-
-    let mut HP_SPRITE_MAP: Vec<&Sprite> = Vec::new();
-    HP_SPRITE_MAP.push(hp_8_sprite);
-    HP_SPRITE_MAP.push(hp_7_sprite);
-    HP_SPRITE_MAP.push(hp_6_sprite);
-    HP_SPRITE_MAP.push(hp_5_sprite);
-    HP_SPRITE_MAP.push(hp_4_sprite);
-    HP_SPRITE_MAP.push(hp_3_sprite);
-    HP_SPRITE_MAP.push(hp_2_sprite);
-    HP_SPRITE_MAP.push(hp_1_sprite);
-
     // Spell effects
     let mut spell_effect = object.object_sprite(SKULL_SPRITE_TAG.sprite(0));
     spell_effect.set_position((170, 100));
@@ -141,10 +120,10 @@ fn game_main(mut gba: agb::Gba) -> ! {
     // Player health bar todo put into a struct together with above?
     // todo having more than one hp bar hides all the character sprites?
     println!("Create first health bar");
-    let mut hp0 = HealthBar::new(&object, &HP_SPRITE_MAP, 28, 16);
-    let mut hp1 = HealthBar::new(&object, &HP_SPRITE_MAP, 28, 80);
-    let mut hp2 = HealthBar::new(&object, &HP_SPRITE_MAP, 100, 16);
-    let mut hp3 = HealthBar::new(&object, &HP_SPRITE_MAP, 100, 80);
+    let mut hp0 = HealthBar::new(&object, 28, 16);
+    let mut hp1 = HealthBar::new(&object, 28, 80);
+    let mut hp2 = HealthBar::new(&object, 100, 16);
+    let mut hp3 = HealthBar::new(&object, 100, 80);
 
     // Frame
     let mut frame = Frame::new(&object, 0, 0);
